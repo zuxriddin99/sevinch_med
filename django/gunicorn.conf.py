d1bd56e5ce2e839d9bf405926670bee47a92451c
zukhriddin <zux.828@gmail.com>
2024-10-21 22:00:17 +0500

    update gunicorn

--- django/gunicorn.conf.py
+++ django/gunicorn.conf.py
@@ -2,7 +2,7 @@ import multiprocessing
 
 wsgi_app = "conf.wsgi"
 limit_request_line = 0
-chdir = "/django/"
+chdir = "/"
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 max_requests = 1000
